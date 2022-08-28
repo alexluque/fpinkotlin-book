ab4fb69b56b77bab9fb58aa054292572b6fd11b5
Alex Luque <alejandro.golu@gmail.com>
2022-08-28 20:50:21 +0200

    Solution for exercise 2.1

--- src/test/kotlin/chapter2/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter2/exercises/ex1/listing.kt
@@ -3,18 +3,20 @@ package chapter2.exercises.ex1
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.WordSpec
 import kotlinx.collections.immutable.persistentMapOf
-import utils.SOLUTION_HERE
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise1 : WordSpec({
     //tag::init[]
-    fun fib(i: Int): Int =
-
-        SOLUTION_HERE()
+    fun fib(i: Int): Int {
+        tailrec fun go(acc: Int, current: Int, next: Int): Int =
+            if (acc == 0) current
+            else go(acc - 1, next, current + next)
+        return go(i, 0, 1)
+    }
     //end::init[]
 
     "fib" should {
-        "!return the nth fibonacci number" {
+        "return the nth fibonacci number" {
             persistentMapOf(
                 1 to 1,
                 2 to 1,
